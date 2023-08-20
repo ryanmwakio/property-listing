af79f0f93875a84472eda6564fb6ef0d85fe075e
Ryan Mwakio <ryanmwakio6@gmail.com>
2023-08-21 01:59:58 +0300

    docs url update

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   let title = "Urbancribs";
-  let rootUrl = "127.0.0.1:8000";
-  let rootApiUrl = "127.0.0.1:8000/api/v1";
+  let rootUrl = "https://urbancribs.onrender.com";
+  let rootApiUrl = "https://urbancribs.onrender.com/api/v1";
 
   res.render("index", { title, rootUrl, rootApiUrl });
 });
